c81c196977425ea7cc9b039b355d7285e0f66530
billyparmenter <bparmenter6387@conestogac.on.ca>
2022-08-16 21:28:13 -0400

    Created posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+10.times do |x|
+  Post.create(title: "Title #{x}", body: "Body #{x} words")
+end
